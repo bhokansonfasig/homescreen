696ed86bb98856a29a1bf5ff6eb0e731db33cbe4
Ben Hokanson-Fasig <bhokansonfasig@gmail.com>
2017-06-21 17:54:42 -0500

    Set up welcome view

--- infotiles/views.py
+++ infotiles/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
-# Create your views here.
+
+class DesktopView(TemplateView):
+    """View class for desktop-based tile layout"""
+    template_name = "infotiles/desktop.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(DesktopView,self).get_context_data(**kwargs)
+        context['name'] = "Ben"
+        return context
